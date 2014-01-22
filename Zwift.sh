f32175b6911f6bf689853ce59bab3cba3e989396
Cody Bunch <bunchc@gmail.com>
2014-01-22 15:24:17 -0600

    fixed screen & apache

--- Zwift.sh
+++ Zwift.sh
@@ -3,8 +3,7 @@ export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
 sudo apt-get install screen vim curl wget git -y
 
-git clone https://github.com/rpedde/zwift-aio.git /opt/zwift-aio
+git clone https://github.com/bunchc/design-summit.git -b "apacheFixes" /opt/design-summit
 
-sudo chmod +x /opt/zwift-aio/*
-sudo /opt/zwift-aio/bootstrap.sh
-sudo /opt/zwift-aio/rescreen.sh
+sudo chmod +x /opt/design-summit/zwift-aio/bootstrap.sh
+sudo /opt/design-summit/zwift-aio/bootstrap.sh
