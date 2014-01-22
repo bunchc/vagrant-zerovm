76befbc2a5e0a21eb580f26c2127ec60d729777f
Cody Bunch <bunchc@gmail.com>
2014-01-22 13:38:53 -0600

    fixed mkdir

--- Zwift.sh
+++ Zwift.sh
@@ -1,9 +1,8 @@
 export DEBIAN_FRONTEND=noninteractive
 
 sudo apt-get update
-sudo apt-get install vim curl wget git -y
+sudo apt-get install screen vim curl wget git -y
 
-mkdir -P /opt/zwift-aio
 git clone https://github.com/rpedde/zwift-aio.git /opt/zwift-aio
 
 sudo chmod +x /opt/zwift-aio/*
