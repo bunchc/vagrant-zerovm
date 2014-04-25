6ab8ed4199918d3e5a1f5f598c37a62c43071a0b
Cody Bunch <bunchc@gmail.com>
2014-04-25 10:50:13 -0500

    fixed the fixed fixes

--- Zwift.sh
+++ Zwift.sh
@@ -3,7 +3,7 @@ export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
 sudo apt-get install screen vim curl wget git -y
 
-git clone https://github.com/bunchc/design-summit.git -b "apacheFixes" /opt/design-summit
+git clone https://github.com/ludditry/design-summit.git /opt/design-summit
 
-sudo chmod +x /opt/design-summit/zwift-aio/bootstrap.sh
-sudo /opt/design-summit/zwift-aio/bootstrap.sh
+sudo chmod +x /opt/design-summit/configuration/zwift-aio/bootstrap.sh
+sudo /opt/design-summit/configuration/zwift-aio/bootstrap.sh
